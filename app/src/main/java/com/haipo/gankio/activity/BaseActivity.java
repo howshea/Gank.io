1a4312923577b0456136d6d08ee55b6b5e97026c
howshea <863229126@qq.com>
2016-10-11 17:47:19 +0800

    搭好基本UI结构

---
+++ app/src/main/java/com/haipo/gankio/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.haipo.gankio.activity;
+
+import android.support.v4.app.Fragment;
+
+public class BaseActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
